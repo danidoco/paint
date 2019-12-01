701c1e05b12ddc0cf3078895032af42652b5acc9
danidoco <oldp79@gmail.com>
2019-12-01 21:56:37 +0900

    Add: mode modifier

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const canvas = document.querySelector(".canvas");
 const ctx = canvas.getContext("2d");
 
 let painting = false;
+let filling = false;
 
 canvas.width = 660;
 canvas.height = 640;
@@ -10,24 +11,24 @@ ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 1;
 
 function draw(event) {
-   const x = event.offsetX;
-   const y = event.offsetY;
+  const x = event.offsetX;
+  const y = event.offsetY;
 
-   if (!painting) {
-      ctx.beginPath();
-      ctx.moveTo(x, y);
-   } else {
-      ctx.lineTo(x, y);
-      ctx.stroke();
-   }
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 function beginPainting(event) {
-   painting = true;
+  painting = true;
 }
 
 function endPainting(event) {
-   painting = false;
+  painting = false;
 }
 
 canvas.addEventListener("mousemove", draw);
@@ -35,12 +36,35 @@ canvas.addEventListener("mousedown", beginPainting);
 canvas.addEventListener("mouseup", endPainting);
 canvas.addEventListener("mouseleave", endPainting);
 
-const colors = document.querySelectorAll(".color")
+const colors = document.querySelectorAll(".color");
 
 function handleColor(event) {
-   ctx.strokeStyle = event.target.style.backgroundColor
+  ctx.strokeStyle = event.target.style.backgroundColor;
 }
 
-Array.from(colors).forEach((color) => {
-   color.addEventListener("click", handleColor)
-})
+Array.from(colors).forEach(color => {
+  color.addEventListener("click", handleColor);
+});
+
+const modeBtn = document.querySelector(".mode");
+
+function handleModeClick() {
+  if (filling) {
+    filling = false;
+    modeBtn.innerText = "FILL";
+  } else {
+    filling = true;
+    modeBtn.innerText = "DRAW";
+  }
+}
+
+function fill() {
+  if (filling) {
+    ctx.fillStyle = ctx.strokeStyle;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
+canvas.addEventListener("click", fill);
+
+modeBtn.addEventListener("click", handleModeClick);
